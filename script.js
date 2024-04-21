4b0873218465b1b9517d468d902abe4d929b430e
TutuSilva <160740774+TutuSilva@users.noreply.github.com>
2024-04-21 11:00:41 -0300

    cabecalho + main mobile feitos

---
+++ script.js
@@ -0,0 +1,10 @@
+function hamborguinho() {
+  let menuAtivo = document.querySelector("#checkbox__menuhamburguer");
+  let itensMenu = document.querySelector("#menuzinho__items");
+  if (menuAtivo.checked == true) {
+    itensMenu.style.display = "flex";
+    itensMenu.style.flexDirection = "column";
+  } else {
+    itensMenu.style.display = "none";
+  }
+}
